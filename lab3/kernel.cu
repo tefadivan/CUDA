2824ae71e926c3c6a67b3eb79a66ac479bf0514f
tefadivan <43060880+tefadivan@users.noreply.github.com>
2020-11-17 17:20:45 +0400

    Create kernel.cu

--- lab3/kernel.cu
+++ lab3/kernel.cu
@@ -6,7 +6,7 @@
 #include <time.h>
 
 #define MAX_SHIFT 2
-#define BLOCK_SIZE 512
+#define BLOCK_SIZE 256
 #define SHIFT 3
 using namespace std;
 
@@ -60,48 +60,62 @@ void findShifts(float* a, float* b, int& host_res, int n, int m) {
 }
 
 __global__ void kernel(float* dev_a, float* dev_b, int* dev_res, int n) {
-	//int row = blockIdx.y * blockDim.y + threadIdx.y;
-	int col = blockIdx.x * blockDim.x + threadIdx.x;
+	int row = blockIdx.x * blockDim.x + threadIdx.x;
 	int dev_res_private = 1;
 	__shared__ int dev_res_shared;
 	for (int j = 0; j < n; ++j) {
-		dev_res_private &= dev_b[col * n + j] == dev_a[col * n + (j + SHIFT) % n];
+		dev_res_private &= dev_b[row * n + j] == dev_a[row * n + (j + SHIFT) % n];
 	}
 	if (threadIdx.x == 0) dev_res_shared = 1;
 	__syncthreads();
+	//dev_res_shared &= dev_res_private;
 	atomicAnd(&dev_res_shared, dev_res_private);
 	__syncthreads();
-	if (threadIdx.x == 0)
-		atomicAnd(dev_res, dev_res_private);
+	if (threadIdx.x == 0) {
+		//*dev_res &= dev_res_shared;
+		atomicAnd(dev_res, dev_res_shared);//(dev_res,dev_res_private)
+	}
 }
+
+__global__ void kernel_shared(float* dev_a, float* dev_b, int* dev_res, int n) {
+	int tx = threadIdx.x, bx = blockIdx.x;
+	int row = bx * blockDim.x + tx;
+	int dev_res_private = 1;
+	__shared__ int dev_res_shared;
+	__shared__ float cache_a[256][32], cache_b[256][32];
+	for (int k = 0; k < n/32; k++) {
+		//заполняем кеши
+		for (int p = 0; p < 32; p++ ){
+			cache_a[tx/32 + p*8][tx%32] = dev_a[];
+			cache_b[tx/32 + p*8][tx%32] = dev_b[];
+		}
+		for (int j = 0; j < 256; ++j) {
+			dev_res_private &= cache_b[bx * blockDim.x][j] == cache_a[bx * blockDim.x][j];
+		}
+	}
+	if (threadIdx.x == 0) dev_res_shared = 1;
+	__syncthreads();
+	//dev_res_shared &= dev_res_private;
+	atomicAnd(&dev_res_shared, dev_res_private);
+	__syncthreads();
+	if (threadIdx.x == 0) {
+		//*dev_res &= dev_res_shared;
+		atomicAnd(dev_res, dev_res_shared);//(dev_res,dev_res_private)
+	}
+}
+
+
 
 int main(int argc, char** argv)
 {
-	int device_count;
-	cudaDeviceProp dp;
-	cudaGetDeviceCount(&device_count);
-	cout << "CUDA device count: " << device_count << "\n";
-	for (int i=0; i<device_count; i++)
-	{
-		cudaGetDeviceProperties(&dp, i);
-		cout << i << ": " << dp.name << " with CUDA compute compatibility " <<
-				dp.major << "." << dp.minor << "\n";
-		cout << "maxGridSize : " << dp.maxGridSize[0] << "\t"
-								   << dp.maxGridSize[1] << "\t"
-								   << dp.maxGridSize[2] << "\t"<< endl;
-		cout << "maxThreadsDim : " << dp.maxThreadsDim[0] << "\t"
-								   << dp.maxThreadsDim[1] << "\t"
-								   << dp.maxThreadsDim[2] << "\t"<< endl;
-
-	}
-	cout << endl;
-
-
-	int n = 500, m = 50000, shift = 1;
+	int n = 1024, m = 26500, shift = 1;
 	float* a = generateSimpleCyclicMatrix(n, m);
 	float* b = generateCyclicShiftMatrix(a, n, m, shift);
-	int res_CPU = true, * res_GPU = new int[1]{1};
-
+	int res_CPU = true, *res_GPU = new int[1];
+	res_GPU[0] = true;
+	cudaEvent_t startCUDA, stopCUDA;
+	cudaEventCreate(&startCUDA);
+	cudaEventCreate(&stopCUDA);
 	clock_t startCPU;
 	float elapsedTimeCUDA, elapsedTimeCPU;
 	startCPU = clock();
@@ -121,11 +135,8 @@ int main(int argc, char** argv)
 	cudaMemcpy(dev_b, b, sz, cudaMemcpyHostToDevice);
 	cudaMemcpy(dev_res, res_GPU, sz_shifts, cudaMemcpyHostToDevice);
 
-	cudaEvent_t startCUDA, stopCUDA;
-	cudaEventCreate(&startCUDA);
-	cudaEventCreate(&stopCUDA);
 	cudaEventRecord(startCUDA, 0);
-	kernel <<<gridSize, blockSize>>> (dev_a, dev_b, dev_res, n);
+	kernel_shared <<<gridSize, blockSize>>> (dev_a, dev_b, dev_res, n);
 	cudaEventRecord(stopCUDA, 0);
 	cudaEventSynchronize(stopCUDA);
 	cudaEventElapsedTime(&elapsedTimeCUDA, startCUDA, stopCUDA);
